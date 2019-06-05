14f1fd5cdbd546cffcc37859ad695e0bee15851e
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-06-05 01:19:27 +0100

    Copy pasta picamera example

--- sense.py
+++ sense.py
@@ -16,18 +16,69 @@ def main():
     args = argz(sys.argv[1:], description=description)
     print(args)
     print(args["resolution"], type(args["resolution"]))
+
+    main_loop(args)
     # create camera object and only continue if it is still valid
-    try:
-        import picamera
-        with picamera.PiCamera() as cam:
-            cam.resolution = (3280, 2464)
-            cam.rotation = 180
-            cam.start_preview()
-            while(True):
-                time.sleep(5)
-                cam.capture(str(time.strftime("%Y-%m-%d_%H:%M:%S", time.gmtime())), 'png')
-    except:
-        print("failed")
+    # try:
+    #     import picamera
+    #     with picamera.PiCamera() as cam:
+    #         cam.resolution = (3280, 2464)
+    #         cam.rotation = 180
+    #         cam.start_preview()
+    #         while(True):
+    #             time.sleep(5)
+    #             cam.capture(str(time.strftime("%Y-%m-%d_%H:%M:%S", time.gmtime())), 'png')
+    # except:
+    #     print("failed")
+
+def main_loop(args):
+    import io
+    import random
+    import picamera
+    from PIL import Image
+
+    prior_image = None
+
+    def detect_motion(camera):
+        global prior_image
+        stream = io.BytesIO()
+        camera.capture(stream, format='jpeg', use_video_port=True)
+        stream.seek(0)
+        if prior_image is None:
+            prior_image = Image.open(stream)
+            return False
+        else:
+            current_image = Image.open(stream)
+            # Compare current_image to prior_image to detect motion. This is
+            # left as an exercise for the reader!
+            result = random.randint(0, 10) == 0
+            # Once motion detection is done, make the prior image the current
+            prior_image = current_image
+            return result
+
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        stream = picamera.PiCameraCircularIO(camera, seconds=10)
+        camera.start_recording(stream, format='h264')
+        try:
+            while True:
+                camera.wait_recording(1)
+                if detect_motion(camera):
+                    print('Motion detected!')
+                    # As soon as we detect motion, split the recording to
+                    # record the frames "after" motion
+                    camera.split_recording('after.h264')
+                    # Write the 10 seconds "before" motion to disk as well
+                    stream.copy_to('before.h264', seconds=10)
+                    stream.clear()
+                    # Wait until motion is no longer detected, then split
+                    # recording back to the in-memory circular buffer
+                    while detect_motion(camera):
+                        camera.wait_recording(1)
+                    print('Motion stopped!')
+                    camera.split_recording(stream)
+        finally:
+            camera.stop_recording()
 
 def resolution(s):
     try:
@@ -61,7 +112,7 @@ def argz(argv, description=None):
     args = vars(parser.parse_args(argv))
 
     # list args which are paths to be made cross platform
-    pathArgNames = ["data"]
+    pathArgNames = ["out_folder"]
 
     # return normalised args with cross platform paths
     return normArgs(args=args, pathArgs=pathArgNames)
