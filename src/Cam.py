2bbb5f61bf8aac152d03fee51794d0868f6ddb58
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-06-10 16:24:32 +0100

    Fixed Cam.py
    accidental duplicate header

--- src/Cam.py
+++ src/Cam.py
@@ -1,26 +1,7 @@
 # @Author: archer
 # @Date:   2019-06-10T10:52:23+01:00
 # @Last modified by:   archer
-# @Last modified time: 2019-06-10T16:21:34+01:00
-
-import sys, os
-
-class Cam():
-    """
-    Raspberry pi Camera V2 abstraction library
-
-    This Class fascilitates high level control of the camera module so that the
-    minor points can just be abstracted away and kept cleanly in a class
-    """
-    import picamera
-
-    def __init__(self, args={}):
-        """
-        __init__ can take an argument dict with state overides.
-# @Author: archer
-# @Date:   2019-06-10T10:52:23+01:00
-# @Last modified by:   archer
-# @Last modified time: 2019-06-10T16:21:34+01:00
+# @Last modified time: 2019-06-10T16:24:25+01:00
 
 import sys, os
 
