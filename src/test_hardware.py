f40c6a7e97841a56960ad3297cc9a78cca8ce033
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-06-24 11:45:55 +0100

    Updated test_hardware indentation

--- src/test_hardware.py
+++ src/test_hardware.py
@@ -1,7 +1,7 @@
 # @Author: archer
 # @Date:   2019-06-24T11:38:32+01:00
 # @Last modified by:   archer
-# @Last modified time: 2019-06-24T11:42:12+01:00
+# @Last modified time: 2019-06-24T11:45:49+01:00
 
 # this file is a test file to check if example code can run on any given
 # hardware as existing Cam.py struggles to run longer than 7 minutes on pi0w
@@ -23,12 +23,12 @@ import picamera
 
 with picamera.PiCamera() as camera:
 
-camera.start_preview()
+    camera.start_preview()
 
-camera.start_recording("/home/alarm/git/raspberry_"+ date + "_video.mjpeg")
+    camera.start_recording("/home/alarm/git/raspberry_"+ date + "_video.mjpeg")
 
-camera.wait_recording(3000)
+    camera.wait_recording(3000)
 
-camera.stop_recording()
+    camera.stop_recording()
 
-camera.stop_preview()
+    camera.stop_preview()
