e17c174a59377fcf55b7cd63f7d87f2ca5de902e
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-06-24 12:05:37 +0100

    Updated test hardware to output in current dir

--- src/test_hardware.py
+++ src/test_hardware.py
@@ -1,7 +1,7 @@
 # @Author: archer
 # @Date:   2019-06-24T11:38:32+01:00
 # @Last modified by:   archer
-# @Last modified time: 2019-06-24T11:58:15+01:00
+# @Last modified time: 2019-06-24T12:05:30+01:00
 
 # this file is a test file to check if example code can run on any given
 # hardware as existing Cam.py struggles to run longer than 7 minutes on pi0w
@@ -25,10 +25,10 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
 
     while True:
-        
+
         date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
-        camera.start_recording("/home/alarm/git/raspberry_"+ date + "_video.mjpeg")
+        camera.start_recording(date + "_video.mjpeg")
 
         camera.wait_recording(60)
 
