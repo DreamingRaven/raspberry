15263c4af01fd5f37382ad869460952b14e49ce6
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-06-26 10:51:29 +0100

    Changing hardware test output

--- src/test_hardware.py
+++ src/test_hardware.py
@@ -1,7 +1,7 @@
 # @Author: archer
 # @Date:   2019-06-24T11:38:32+01:00
 # @Last modified by:   archer
-# @Last modified time: 2019-06-24T12:11:31+01:00
+# @Last modified time: 2019-06-25T00:01:57+01:00
 
 # this file is a test file to check if example code can run on any given
 # hardware as existing Cam.py struggles to run longer than 7 minutes on pi0w
@@ -11,14 +11,7 @@ import datetime
 
 import time
 
-
-
-
-
 import picamera
-
-
-
 
 with picamera.PiCamera() as camera:
 
@@ -28,7 +21,7 @@ with picamera.PiCamera() as camera:
 
         date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") #"%m_%d_%Y_%H_%M_%S")
 
-        camera.start_recording(date + "_video.mjpeg")
+        camera.start_recording(date + ".mjpeg")
 
         camera.wait_recording(60)
 
