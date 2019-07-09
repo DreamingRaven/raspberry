5523b5e145e205d3d11dc6ee2f5918d2cbb6fc77
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-07-09 10:43:19 +0100

    Removed sense.py and updated logging in test
    hardware test now prints out the datetime for each camera recording.
    since we no longer will need sense.py i have also removed that as it was
    just a gaff experiment at the time

--- src/test_hardware.py
+++ src/test_hardware.py
@@ -1,7 +1,7 @@
 # @Author: archer
 # @Date:   2019-06-24T11:38:32+01:00
 # @Last modified by:   archer
-# @Last modified time: 2019-06-25T00:01:57+01:00
+# @Last modified time: 2019-07-04T14:15:46+01:00
 
 # this file is a test file to check if example code can run on any given
 # hardware as existing Cam.py struggles to run longer than 7 minutes on pi0w
@@ -20,7 +20,7 @@ with picamera.PiCamera() as camera:
     while True:
 
         date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") #"%m_%d_%Y_%H_%M_%S")
-
+        print(date)
         camera.start_recording(date + ".mjpeg")
 
         camera.wait_recording(60)
