5fabbce9296e12678511423289678f783624f10e
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-07-21 22:11:37 +0100

    Added panda_sensors.py to put sensors in pandas
    sensors init outlined still need rest

---
+++ src/panda_sensors.py
@@ -0,0 +1,77 @@
+# @Author: archer
+# @Date:   2019-07-21T21:14:14+01:00
+# @Last modified by:   archer
+# @Last modified time: 2019-07-21T22:10:50+01:00
+
+
+from __future__ import print_function, absolute_import   # python 2-3 compat
+import os
+import time
+import datetime
+
+
+class Sense(object):
+    """Raspberry Pi specific class to put sensor data into pandas files."""
+
+    def __init__(self, args=None, logger=None):
+        """Create object and test what is availiable."""
+        args = args if args is not None else dict()
+        self.home = os.path.expanduser("~")
+        defaults = {
+            "cam_import_attempts": 2,
+            "cam_resolution": "groot",
+            "pylog": logger if logger is not None else print,
+        }
+        self.args = self._merge_dicts(defaults, args)
+
+    __init__.__annotations__ = {"args": dict, "return": None}
+
+    def debug(self):
+        """Log function to help track the internal state of the class."""
+        self.args["pylog"](self.args)
+
+    debug.__annotations__ = {"return": None}
+
+    def _merge_dicts(self, *dicts):
+        """Given multiple dictionaries, merge together in order."""
+        result = {}
+        for dictionary in dicts:
+            result.update(dictionary)  # merge each dictionary in order
+        return result
+
+    _merge_dicts.__annotations__ = {"dicts": dict, "return": None}
+
+    def _init_camera(self):
+        """Check that camera library and camera can be used."""
+        # a quick note the camera may fail the first time around
+        # so we need to init it twice thanks to poor lower level code
+        try:  # try to import camera
+            import picamera
+            for _ in range(self.args["cam_import_attempts"]):
+                try:  # try to init camera _ times
+                    self.args["cam"] = picamera.PiCamera()
+                    return True
+                except picamera.exc.PiCameraMMALError:
+                    self.args["pylog"]("warning: unable to start camera")
+        except ImportError:
+            self.args["pylog"]("warning: unable to import picamera")
+        self.args["pylog"]("warning: skipping camera")
+        return False
+
+    _init_camera.__annotations__ = {"return": bool}
+
+    def _init_bme680(self):
+        """Check that bme680 library and bme680 can be used."""
+        return False
+
+    _init_bme680.__annotations__ = {"return": bool}
+
+
+def test():
+    sensors = Sense()
+    sensors._init_camera()
+    sensors.debug()
+
+
+if(__name__ == "__main__"):
+    test()
