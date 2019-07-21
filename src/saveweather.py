a31e617428e1a6d91ddc7df21bd66d1cc17910e7
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-07-21 20:51:23 +0100

    Enapsulated bme680 but still testing
    still testing use of bme680 and what error is specifically thrown.

--- src/saveweather.py
+++ src/saveweather.py
@@ -1,4 +1,3 @@
-import bme680
 import time
 import datetime
 
@@ -12,13 +11,16 @@ try:
 except ImportError:
     print("warning: unable to use camera, picmera does not exist")
 
-sensor = bme680.BME680()
+try:
+    import bme680
+except ImportError:
+    print("warning: unable to use bme680, import bme680 failed")
 
+sensor = bme680.BME680()
 sensor.set_humidity_oversample(bme680.OS_2X)
 sensor.set_pressure_oversample(bme680.OS_4X)
 sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
-
 sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
 sensor.set_gas_heater_temperature(320)
 sensor.set_gas_heater_duration(150)
